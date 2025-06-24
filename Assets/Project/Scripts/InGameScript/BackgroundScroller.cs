d9852c6cfd478f8cbd52995ec1f00cd51d168142
yuuyu-17 <20122017@kaishi-pu.ac.jp>
2025-06-24 16:27:46 +0900

    インゲーム背景をループから左に流れるように変更

--- Assets/Project/Scripts/InGameScript/BackgroundScroller.cs
+++ Assets/Project/Scripts/InGameScript/BackgroundScroller.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    public float backgroundWidth; // 背景画像の幅
 
     private void Update()
     {
-        BackGroundScroll();
+        ScrollBackground();
     }
 
-    private void BackGroundScroll()
+    private void ScrollBackground()
     {
         // InGameManagerから現在のスクロール速度を取得
         float currentScrollSpeed = 0f;
@@ -23,13 +22,7 @@ public class BackgroundScroller : MonoBehaviour
             currentScrollSpeed = 1.0f; // InGameManagerが見つからない場合、デフォルトの速度を設定
         }
 
-        // 背景を左に移動させる
+        // 背景を左に移動させる（ここが変更なしでメインの処理）
         transform.Translate(Vector3.left * currentScrollSpeed * Time.deltaTime);
-
-        // 背景が画面の左端より外に出たら、右端に移動してループさせる
-        if (transform.position.x < -backgroundWidth)
-        {
-            transform.Translate(Vector3.right * backgroundWidth * 2);
-        }
     }
 }
